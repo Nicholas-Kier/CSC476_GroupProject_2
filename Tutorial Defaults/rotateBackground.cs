3d0e68e583832cb1c5ae9026e0c914b28432d0d5
Nicholas Kier <nicholas.k.kier@gmail.com>
2020-12-01 16:25:47 -0500

    2nd commit

---
+++ Tutorial Defaults/rotateBackground.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateBackground : MonoBehaviour
+{
+    float speed = 1.17f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+}
