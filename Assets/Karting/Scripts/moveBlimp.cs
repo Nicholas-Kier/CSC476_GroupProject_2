0133f351ec1783e62fb4aa570a06371c5a3b00a8
Nicholas Kier <nicholas.k.kier@gmail.com>
2020-12-03 21:32:56 -0500

    Added a blimp!
    wew

---
+++ Assets/Karting/Scripts/moveBlimp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBlimp : MonoBehaviour
+{
+    [SerializeField] private Vector3 target = new Vector3(1, 1, 1);
+    [SerializeField] private float speed = 1;
+
+    private void Update()
+    {
+        // Moves the object to target position
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+    }
+}
